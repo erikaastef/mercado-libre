0caf3406d78839cfc15074458fbbc0d0e58bc80c
Erika Stefania Castillo Barragan <erikaastef99@192.168.1.2>
2021-01-18 09:04:43 -0300

    global styles, reset styles, theme

---
+++ styles/Theme.js
@@ -0,0 +1,20 @@
+export const theme = {
+    colors: {
+        white: "#FFFFFF",
+        yellow: "#FFE600",
+        black: "#333333",
+        darkGray: "#666666",
+        gray: "#999999",
+        lightGray: "#EEEEEE",
+        blue: "#3483FA",
+
+    },
+    device: {
+        xsm: '450px',
+        sm: '576px',
+        md: '768px',
+        lg: '992px',
+        xl: '1200px',
+        xxl: '1440px'
+    }
+}
