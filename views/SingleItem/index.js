f06897bda805a634887392b626a998e797213474
Erika Stefania Castillo Barragan <erikaastef99@192.168.1.5>
2021-02-26 03:37:23 -0300

    grid fix

--- views/SingleItem/index.js
+++ views/SingleItem/index.js
@@ -19,19 +19,19 @@ export default function SingleItem({ item }) {
         <>
             {!objectIsEmpty(item) &&
                 <Wrapper>
-                    <FullInfo>
-                        <Photo src={item.picture} />
+                    <Photo src={item.picture} />
+                    {item.description ?
                         <Description>
                             <h2>Descripcion del producto</h2>
                             <p>{item.description}</p>
-                        </Description>
-                    </FullInfo>
+                        </Description> : ''}
                     <Summary>
                         <span>{`${translate(item?.condition)} - ${item?.sold_quantity} Vendidos`}</span>
                         <h3>{item.title}</h3>
                         <h1>{currencyFormat(item?.price?.amount)}</h1>
-                        <Button type="secondary">Comprar</Button>
+                        <Button type="secondary" className="btn-web">Comprar</Button>
                     </Summary>
+                    <Button type="secondary" className="btn-mobile">Comprar</Button>
                 </Wrapper>
             }
         </>
--- views/SingleItem/styles.js
+++ views/SingleItem/styles.js
@@ -3,37 +3,50 @@ import styled from 'styled-components'
 export const Wrapper = styled.div`
     display:grid;
     grid-template-columns:1.5fr 0.5fr;
-    grid-template-areas: "fullInfo summary";
+    grid-template-areas: 
+    "photo summary"
+    "description auto";
     gap:20px;
     padding:32px;
+    .btn-mobile{
+        display:none;
+        grid-area:button
+    }
     @media(max-width:${({ theme }) => theme.device.md}){
+        .btn-web{
+            display:none;
+        }
+        .btn-mobile{
+            display:block;
+        }
         grid-template-columns:1fr;
         grid-template-areas: 
         "summary"
-        "fullInfo";
+        "photo"
+        "description"
+        "button";
         gap:32px;
 
     }
 `
-export const FullInfo = styled.div`
-    grid-area:fullInfo;
-    display:flex;
-    flex-direction:column;
-    justify-content:space-between;
-`
+
 export const Photo = styled.img`
-    max-width:680px;
+    grid-area:photo;
+    max-width:600px;
+    min-width:200px;
     width:100%;
 `
 export const Description = styled.div`
+    grid-area:description;
     h2{
         font-size:28px;
-        margin-bottom:32px;
+        margin: 32px 0px;
     }
     p{
         color:${({ theme }) => theme.colors.gray};
         font-size:16px;
     }
+    overflow:auto;
 `
 
 export const Summary = styled.div`
